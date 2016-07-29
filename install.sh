6469c3c2e1b24c75327d6b9dc3b61924535486f8
joao brito junior <juniorjbn@gmail.com>
2016-07-29 17:59:56 -0300

    Update install.sh
    ops

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt-sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
 
 apt-get purge lxc-docker && apt-get update && apt-get install apt-transport-https ca-certificates linux-image-extra-$(uname -r)
 
